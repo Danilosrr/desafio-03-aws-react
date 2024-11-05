2e51be35f7d95fb3bc9187fb10e80137a2bbdc80
Danilosrr <danilo_srr@hotmail.com>
2024-11-04 22:24:32 -0300

    refactor: react-hook-form and css

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,19 +1,46 @@
 import { FaArrowRight } from "react-icons/fa";
 import { TbBrandGithubFilled } from "react-icons/tb";
+import { useForm } from "react-hook-form";
 import "./Search.css";
 
+interface IFormInput {
+  search: string;
+}
+
 export default function Search() {
+  const {
+    register,
+    setError,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInput>();
+
+  const onSubmit = (data: IFormInput) => {
+    //setError("search", { type:"server", message:"O nome que você digitou não está cadastrado!" });
+    console.log(JSON.stringify(data));
+  };
+
   return (
     <main className="searchContainer">
       <h2>Digite o nome do usuário que deseja buscar</h2>
       <section className="searchBar">
-        <input placeholder="Digite o nome do usuário" />
-        <button className="primary">
-          <FaArrowRight size={25} />
-        </button>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input
+            {...register("search", {
+              required: {value: true, message: "O nome que você digitou não existe ou não está cadastrado!"}
+            })}
+          />
+          
+          <button className="primary" type="submit">
+            <FaArrowRight size={25} />
+          </button>
+        </form>
+        {errors.search  &&  <p className="warning">{errors.search.message}</p>}
       </section>
       <section className="line">
-        <hr /><b>ou</b><hr />
+        <hr />
+        <b>ou</b>
+        <hr />
       </section>
       <section className="login">
         <b>Acesse sua conta com</b>
