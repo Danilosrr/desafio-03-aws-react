c71e2649ac79b72b5aa38af78854dbe0700ce400
Danilosrr <danilo_srr@hotmail.com>
2024-11-09 00:08:08 -0300

    test: test setup, context mockup

--- src/interfaces/github.ts
+++ src/interfaces/github.ts
@@ -4,11 +4,6 @@ export interface GithubUserData {
   name: string;
   avatar_url: string;
   bio: string | null;
-  company: string | null;
   email: string | null;
-  followers: number;
-  following: number;
   location: string | null;
-  public_repos: 53;
-  repos_url: "https://api.github.com/users/Danilosrr/repos";
 }
